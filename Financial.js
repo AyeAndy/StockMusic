964f5ba4508962dab2e8f413109db4bb9b546252
jeremyjchung <jeremyjchung@gmail.com>
2016-04-09 14:05:21 -0400

    UI updated with buttons.

--- Financial.js
+++ Financial.js
@@ -18,81 +18,81 @@ $(document).ready(function() {
         endDate = "2016-01-03";
     }
 
-      var url_string = "https://www.quandl.com/api/v3/datasets/WIKI/" + symbol + "/data.json";
+    var url_string = "https://www.quandl.com/api/v3/datasets/WIKI/" + symbol + "/data.json";
 
-      $.ajax({
-          type:"GET",
-          dataType: "json",
-          url: url_string,
-          data:{
-            api_key: "zzDUgBZpN8za7SvaPCTv",
-            order: "asc",
-            start_date: startDate,
-            end_date: endDate
-          },
-          success: function(resultData){
-            $('#header-symbol').text(symbol);
-            $('#header-date-range').text(startDate + " to " + endDate);
-            $('#header-date-range').text("(" + startDate + " to " + endDate + ")");
-            console.log(resultData);
-            var data = resultData.dataset_data.data;
-            length = data.length;
-            var chartData = {
-              labels: [],
-              datasets:[
-                  {
-                    strokeColor: "rgba(65, 105, 225, 1)",
-                    label: symbol,
-                    data: []
-                  },
-                  {
-                    strokeColor: "rgba(216, 12, 60, 1)",
-                    label: symbol,
-                    data: []
-                  }
-              ]
-            }
-
-            var time1 = 0;
-            var time2 = data.length/4;
-            var time3 = data.length/2;
-            var time4 = data.length*3/4;
-            var time5 = data.length-1;
-
-            for (var i = 0; i < data.length; i++){
-
-                if(i == time1 || i == time2 || i == time3 || i == time4 || i == time5){
-                  var month = Number(data[i][0].split("-")[1]) - 1;
-                  chartData.labels.push(MONTHS[month]);
+    $.ajax({
+        type:"GET",
+        dataType: "json",
+        url: url_string,
+        data:{
+          api_key: "zzDUgBZpN8za7SvaPCTv",
+          order: "asc",
+          start_date: startDate,
+          end_date: endDate
+        },
+        success: function(resultData){
+          $('#header-symbol').text(symbol);
+          $('#header-date-range').text(startDate + " to " + endDate);
+          $('#header-date-range').text("(" + startDate + " to " + endDate + ")");
+          console.log(resultData);
+          var data = resultData.dataset_data.data;
+          length = data.length;
+          var chartData = {
+            labels: [],
+            datasets:[
+                {
+                  strokeColor: "rgba(65, 105, 225, 1)",
+                  label: symbol,
+                  data: []
+                },
+                {
+                  strokeColor: "rgba(216, 12, 60, 1)",
+                  label: symbol,
+                  data: []
                 }
-                else{
-                  chartData.labels.push("");
-                }
-
-                chartData.datasets[0].data.push(data[i][2]);
-                chartData.datasets[1].data.push(data[i][3]);
-            }
-
-            var context = $("#chart").get(0).getContext("2d");
-            var chart = new Chart(context).TickerLine(chartData, null);
-
-            var id = setInterval(function (){
-
-                if(pos == length)
-                    clearInterval(id);
-                else{
-                    chart.clear();
-                    pos++;
-                    chart.update();
-                }
-            }, 10);
-              
-            $('#infoModal').modal("hide");
-          },
-          failure: function(err){
-            console.log(err);
+            ]
           }
-      });
+
+          var time1 = 0;
+          var time2 = data.length/4;
+          var time3 = data.length/2;
+          var time4 = data.length*3/4;
+          var time5 = data.length-1;
+
+          for (var i = 0; i < data.length; i++){
+
+              if(i == time1 || i == time2 || i == time3 || i == time4 || i == time5){
+                var month = Number(data[i][0].split("-")[1]) - 1;
+                chartData.labels.push(MONTHS[month]);
+              }
+              else{
+                chartData.labels.push("");
+              }
+
+              chartData.datasets[0].data.push(data[i][2]);
+              chartData.datasets[1].data.push(data[i][3]);
+          }
+
+          var context = $("#chart").get(0).getContext("2d");
+          var chart = new Chart(context).TickerLine(chartData, null);
+
+          var id = setInterval(function (){
+
+              if(pos == length)
+                  clearInterval(id);
+              else{
+                  chart.clear();
+                  pos++;
+                  chart.update();
+              }
+          }, 10);
+
+          $('#infoModal').modal("hide");
+        },
+        failure: function(err){
+          console.log(err);
+        }
+    });
   });
 });
 
@@ -119,4 +119,3 @@ Chart.types.Line.extend({
         }
     }
 });
-
