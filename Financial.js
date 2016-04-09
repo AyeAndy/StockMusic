1ff41a48529d4b9a3ccc96b93eb63658b3936a82
Jack Dai <Jack Dai>
2016-04-09 19:44:00 -0400

    Loops the ticker

--- Financial.js
+++ Financial.js
@@ -1,5 +1,4 @@
 var pos = 0;
-var length = 0;
 var MONTHS = ["JAN", "FEB", "MAR", "APRIL", "MAY", "JUNE", "JULY", "AUG", "SEPT","OCT", "NOV", "DEC"]
 
 $(document).ready(function() {
@@ -12,11 +11,12 @@ $(document).ready(function() {
     var startDate = $('#start-date').val();
     var endDate = $('#end-date').val();
 
-    if(symbol == '' || startDate == '' || endDate == ''){
+    if(symbol == "")
         symbol = "AAPL";
+    if(startDate == "")
         startDate = "2015-01-01";
+    if(endDate == "")
         endDate = "2016-01-03";
-    }
 
       var url_string = "https://www.quandl.com/api/v3/datasets/WIKI/" + symbol + "/data.json";
 
@@ -31,10 +31,8 @@ $(document).ready(function() {
             end_date: endDate
           },
           success: function(resultData){
-            $('#header-symbol').text(symbol);
-            $('#header-date-range').text(startDate + " to " + endDate);
+            $('#header-symbol').text(symbol.toUpperCase());
             $('#header-date-range').text("(" + startDate + " to " + endDate + ")");
-            console.log(resultData);
             var data = resultData.dataset_data.data;
             length = data.length;
             var chartData = {
@@ -54,10 +52,10 @@ $(document).ready(function() {
             }
 
             var time1 = 0;
-            var time2 = data.length/4;
-            var time3 = data.length/2;
-            var time4 = data.length*3/4;
-            var time5 = data.length-1;
+            var time2 = Math.floor(data.length/4);
+            var time3 = Math.floor(data.length/2);
+            var time4 = Math.floor(data.length*3/4);
+            var time5 = Math.floor(data.length-1);
 
             for (var i = 0; i < data.length; i++){
 
@@ -78,14 +76,10 @@ $(document).ready(function() {
 
             var id = setInterval(function (){
 
-                if(pos == length)
-                    clearInterval(id);
-                else{
-                    chart.clear();
-                    pos++;
-                    chart.update();
-                }
-            }, 10);
+                chart.clear();
+                pos = (pos + 1) % data.length;
+                chart.update();
+        }, 10);
               
             $('#infoModal').modal("hide");
           },
@@ -109,14 +103,12 @@ Chart.types.Line.extend({
         var point = this.datasets[0].points[pos];
         var scale = this.scale;
 
-        if(pos < length){
-            // draw line
-            this.chart.ctx.beginPath();
-            this.chart.ctx.moveTo(point.x, scale.startPoint + 24);
-            this.chart.ctx.strokeStyle = '#ff0000';
-            this.chart.ctx.lineTo(point.x, scale.endPoint);
-            this.chart.ctx.stroke();
-        }
+        // draw line
+        this.chart.ctx.beginPath();
+        this.chart.ctx.moveTo(point.x, scale.startPoint );
+        this.chart.ctx.strokeStyle = '#ff0000';
+        this.chart.ctx.lineTo(point.x, scale.endPoint);
+        this.chart.ctx.stroke();
     }
 });
 
